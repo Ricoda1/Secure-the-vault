4e3c293ff4c985bea8e8d740c449e8f913f7eedc
Ricoda1 <126440530+Ricoda1@users.noreply.github.com>
2023-03-21 09:57:40 -0700

    Add files via upload
    Block 7 workshop: Secure the vault

---
+++ main.js
@@ -0,0 +1,14 @@
+//1. create the 3 variables for the 3 combination codes
+//2. 
+
+
+const num1 = 5 * 2;
+const num2 = 80 / 2;
+const num3 = 30 + 9;
+
+//create the string with the message 
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// create the pop up 
+alert(" " + message + " " + num1 + " " + num2 + " " + num3);
